cdd83ea3a011967c70fb418337bc6019a5bb8f7a
Rafael Klaessen <rafaelklaessen.nl@gmail.com>
2017-10-05 21:36:27 +0200

    Removed JSDoc types

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ const parseNum = require('parse-num')
 
 /* global Intl */
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
 const defaultOptions = {
   nanZero: true,
   locale: 'en-US',
   localeMatcher: 'best fit',
   useGrouping: true, // grouping separator determined by locale
   maximumFractionDigits: 15
+  // OTHER
+  // minimumIntegerDigits
+  // minimumFractionDigits
+  // maximumFractionDigits
+  // minimumSignificantDigits
+  // maximumSignificantDigits
 }
 
-/**
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NumberFormat
- * @param {number} number Number to format
- * @param {Object} opts Options for formatting
- * @param {boolean} [opts.nanZero=true]
- * @param {string} [opts.locale=en-US]
- * @param {string} [opts.localeMatcher=best fit]
- * @param {boolean} [opts.useGrouping=true]
- * @param {number} [opts.minimumIntegerDigits]
- * @param {number} [opts.minimumFractionDigits]
- * @param {number} [opts.maximumFractionDigits=15]
- * @param {number} [opts.minimumSignificantDigits]
- * @param {number} [opts.maximumSignificantDigits]
- */
 const formatNum = (number, opts) => {
   opts = renameKeyShortcuts(Object.assign({}, defaultOptions, opts))
   number = parseNum(number)
@@ -37,9 +30,6 @@ const formatNum = (number, opts) => {
   return nf.format(number)
 }
 
-/**
- * @param {Object} opts
- */
 const renameKeyShortcuts = (opts) => {
   Object.keys(opts).forEach((key) => {
     expandMin(opts, key)
@@ -51,34 +41,15 @@ const renameKeyShortcuts = (opts) => {
   return opts
 }
 
-/**
- * @param {Object} opts
- * @param {string} key
- */
 const expandMin = (opts, key) => expand(opts, key, 'min', 'minimum')
-
-/**
- * @param {Object} opts
- * @param {string} key
- */
 const expandMax = (opts, key) => expand(opts, key, 'max', 'maximum')
 
-/**
- * @param {Object} opts
- * @param {string} key
- * @param {string} shorthand
- * @param {string} full
- */
 const expand = (opts, key, shorthand, full) => {
   if (!key.includes(full) && key.startsWith(shorthand)) {
     replaceKey(opts, key, key.replace(shorthand, full))
   }
 }
 
-/**
- * @param {Object} opts
- * @param {string} key
- */
 const addDigits = (opts, key) => {
   if (
     (key.startsWith('minimum') || key.startsWith('maximum')) &&
@@ -88,11 +59,6 @@ const addDigits = (opts, key) => {
   }
 }
 
-/**
- * @param {Object} opts
- * @param {string} oldKey
- * @param {string} newKey
- */
 const replaceKey = (obj, oldKey, newKey) => {
   obj[newKey] = obj[oldKey]
   delete obj[oldKey]
